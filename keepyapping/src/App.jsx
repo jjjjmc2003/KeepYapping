f314d12be7968f70a832197e4023df91f4697ea3
ChaseWink05 <155601183+ChaseWink05@users.noreply.github.com>
2025-05-06 11:13:16 -0400

    Password recovery

--- keepyapping/src/App.jsx
+++ keepyapping/src/App.jsx
@@ -4,6 +4,8 @@ import ChatApp from "./ChatApp";
 import Login from "./Login";
 import Signup from "./Signup";
 import HomePage from "./HomePage";
+import PasswordReset from "./PasswordReset";
+import ResetPassword from "./ResetPassword";
 
 
 function App() {
@@ -17,6 +19,8 @@ function App() {
             <>
               <Route path="/signup" element={<Signup />} />
               <Route path="/login" element={<Login onLogin={setUser} />} />
+              <Route path="/password-reset" element={<PasswordReset />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
               <Route path="*" element={<Login onLogin={setUser} />} />
             </>
           ) : (
